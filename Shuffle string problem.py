d652ba714e72e23af77949de0d01affab855c34e
Nahlaelhussien <60172142+Nahlaelhussien@users.noreply.github.com>
2026-02-23 21:40:15 +0200

    Update Shuffle string problem.py

--- Shuffle string problem.py
+++ Shuffle string problem.py
@@ -1,10 +1,6 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        dic={}
-        for i in range(len(indices)):
-            dic[indices[i]]=s[i]
-        sorted_indices=sorted(indices)
-        result=""
-        for key in sorted_indices:
-            result=result+dic[key]
-        return result
+        result = [""] * len(s)
+        for i, index in enumerate(indices):
+            result[index] = s[i]
+        return "".join(result)
