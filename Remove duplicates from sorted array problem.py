e2334922d3847d3b4e31be77e3265a850a6141fb
Nahlaelhussien <60172142+Nahlaelhussien@users.noreply.github.com>
2026-03-05 21:01:35 +0200

    Create Remove duplicates from sorted array problem.py

---
+++ Remove duplicates from sorted array problem.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        p=1 #pointer
+        for i in range(1,len(nums)):
+            if nums[i]!=nums[i-1]:
+                nums[p]=nums[i]
+                p += 1
+        
+        return p
+        
+        
