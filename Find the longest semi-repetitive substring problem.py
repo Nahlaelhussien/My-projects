370e3f2e6018523e515d2315d5f3d77611d01fcb
Nahlaelhussien <60172142+Nahlaelhussien@users.noreply.github.com>
2026-03-25 23:58:22 +0200

    Create Find the longest semi-repetitive substring problem.py

---
+++ Find the longest semi-repetitive substring problem.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestSemiRepetitiveSubstring(self, s: str) -> int:
+        count=0
+        left = 0
+        max_len = 1
+        for right in range(1,len(s)):
+            if s[right] == s[right-1]:
+                count+=1
+            while count > 1:
+                if s[left] == s[left+1]:
+                    count -= 1
+                left += 1
+            max_len = max( max_len, right - left + 1)
+        return max_len
