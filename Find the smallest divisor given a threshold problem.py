64bc423ea78cdaf5503d40799558415989d5d3ee
Nahlaelhussien <60172142+Nahlaelhussien@users.noreply.github.com>
2026-04-24 23:48:47 +0300

    Create Find the smallest divisor given a threshold problem.py

---
+++ Find the smallest divisor given a threshold problem.py
@@ -0,0 +1,17 @@
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        def compute_sum(divisor):
+            return sum(math.ceil(num / divisor) for num in nums)
+
+        left, right = 1, max(nums)
+        result = right
+
+        while left <= right:
+            mid = (left + right) // 2
+            if compute_sum(mid) <= threshold:
+                result = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return result
